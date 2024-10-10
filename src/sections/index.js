1abe4ffdcd3c2cf5352d3c67f9ebedde874c88a0
Subhradip <nandisubhradip01@gmail.com>
2024-10-10 18:52:51 +0530

    First Commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import SpecialOffer from './SpecialOffer';
+import PopularProducts from './PopularProducts';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+
+export{
+    Hero,
+    PopularProducts,
+    Footer,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews
+}
